5e846edf38e1b94db0dfc5d4e94facfa1702d9a3
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-08-03 04:23:26 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
+  },
   server: {
-    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Ensure this matches your backend port
+        target: 'https://jobmarketbackend.onrender.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
       },
     },
   },
+  define: {
+    'process.env': {
+      VITE_BACKEND_URL: JSON.stringify('https://jobmarketbackend.onrender.com'),
+    },
+  },
 });
-
-

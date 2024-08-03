d6cbf8baa65eca681d56881ddd6d62d72157326e
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-08-03 06:22:22 +0100

    sdfooodf

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -14,6 +14,7 @@ export const jobLoader = async ({ params }) => {
 
   try {
     const res = await fetch(`https://jobmarketbackend.onrender.com/api/jobs/${id}`);
+
     if (!res.ok) {
       throw new Error(`Network response was not ok: ${res.statusText}`);
     }

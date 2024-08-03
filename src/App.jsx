2547c4ba67d748213cffdfbe51bc3ef45c85012d
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-08-03 12:05:23 +0100

    god abeg

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 import EditJobPage from './pages/EditJobPage';
+
 const App = () => {
   // Add New Job
   const addJob = async (newJob) => {
@@ -85,11 +86,7 @@ const App = () => {
     )
   );
 
-  return (
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
